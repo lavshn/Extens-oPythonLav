0d89bdfb4841e64c99e37769632d9d10a2da9e82
Lavínia Honório <116286149+lavshn@users.noreply.github.com>
2022-11-21 14:36:18 +0000

    .

---
+++ funcao.py
@@ -0,0 +1,17 @@
+#função
+
+def nome_completo():
+ primeiro_nome = input('Qual seu primeiro nome? ')
+ sobrenome = input('Qual seu sobrenome? ')
+ nome_inteiro = primeiro_nome + ' ' + sobrenome
+ print(nome_inteiro)
+
+nome_completo()
+
+#---------------------------------------------------
+
+def saudacao():
+  nome = input('qual seu nome? ')
+  print(f'Olá {nome}')
+
+saudacao() 
